4a525cb23367df30f685f384a88e40278e50e822
mjsterck <sterckx.matthias@live.com>
2020-01-15 20:34:44 -0500

    Changes floats to strings in output

--- p01/p01_convert_temp.py
+++ p01/p01_convert_temp.py
@@ -1,3 +1,3 @@
-f = input('Enter the temperature in Fahrenheit: ')
-c = 5/9 * (float(f) - 32)
-print(f, 'degrees F is', c, 'degrees C')
+f = float(input('Enter the temperature in Fahrenheit: '))
+c = 5/9 * (f - 32)
+print(str(f), 'degrees F is', str(c), 'degrees C')
